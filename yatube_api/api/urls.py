9d21a7aeb6b293aaefe70dd2b63466e94fd8279f
Resistor-git <partizan-resistor@yandex.ru>
2023-04-22 18:20:09 +0300

    Add psgination for PostViewSet. Add JWT tokens and djoser

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import SimpleRouter
+
+from api.views import PostViewSet
+
+app_name = 'api'
+
+router = SimpleRouter()
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,29 @@
-# TODO:  Напишите свой вариант
+# TODO:
+# Post -list (get, post)
+#   -- auth or readonly
+#   -- limit, offcet
+# Post -detail (get, post, patch, delete)
+#   -- owner
+# Comment -list (get, post)
+# Comment -detail (get, post, patch, delete)
+#  -- owner
+# Follow -list (get)
+#   -- auth(!)
+# Follow -detail ((get, post, patch, delete)
+#   -- auth, request.user = obj.user
+
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Post
+from api.serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
