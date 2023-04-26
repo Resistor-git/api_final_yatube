9632b02673902fda38b1731dba75b63395eb27dd
Resistor-git <partizan-resistor@yandex.ru>
2023-04-26 12:39:32 +0300

    Reviewfix: created patterns for jwt, instead of authomatic djoser patterns

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from api.views import PostViewSet, GroupViewSet, FollowViewSet, CommentViewSet
 
@@ -14,7 +19,17 @@ router.register(r'posts/(?P<post_id>\d+)/comments',
                 CommentViewSet,
                 basename='comments')
 
+jwt_patterns = [
+    path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
+
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(jwt_patterns)),
+    # path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # path('v1/', include('djoser.urls.jwt')), 
 ]
